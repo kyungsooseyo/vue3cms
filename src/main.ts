a27fdcedb08db01862e5d0991b2e6a85e0e16097
wanglimin47 <wanglimin47@jd.com>
2021-11-09 15:41:14 +0800

    feat: 添加axios

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import 'element-plus/dist/index.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './service/axios.demo.ts';
 // import ElementPlus from 'element-plus';
 const app = createApp(App);
 app.use(router);
 app.use(store);
 // app.use(ElementPlus);
 app.mount('#app');
+console.log(process.env.VUE_APP_BASE_NAME); //= 若想在ts中使用，必须在shims-vue.d.ts文件里面声明一下
---
+++ src/service/axios.demo.ts
@@ -0,0 +1,54 @@
+import Axios from 'axios';
+// Axios.get('http://123.207.32.32:8000/home/multidata').then(
+//   (res: AxiosResponse) => {
+//     console.log(res);
+//   }
+// );
+
+// 定义泛型
+// new Promise<string>((resolve) => {
+//   resolve(12);  // ~指定string后，这传入number机会报错
+// }).then((res) => {
+//   console.log(res);
+// });
+// axios 配置选项
+
+Axios.defaults.baseURL = 'http://httpbin.org';
+
+// 等所有请求都有数据后再进行后续操作
+Axios.all([
+  Axios.get('/get', { params: { name: 'why', age: 18 } }),
+  Axios.post('/post', { params: { name: 'why', age: 18 } })
+]).then((res) => {
+  console.log(res); //- 返回的res是个数组
+});
+//= axios拦截器,
+// fn1 请求发送成功时会执行的函数，fn2 请求发送失败时会执行的函数
+// Axios.interceptors.request.use(fn1, fn2);
+// Axios.interceptors.response(fn1, fn2);
+
+Axios.interceptors.request.use(
+  (config) => {
+    // 给请求添加token, 添加loading动画
+
+    return config;
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+// 拿到数据，就不会再进行手动拿data了，这样就可以直接拿到data
+Axios.interceptors.response.use(
+  (res) => {
+    return res.data;
+  },
+  (err) => {
+    console.log('服务器相应失败');
+    return err;
+  }
+);
+
+process.env.NODE_ENV == 's';
+const a = 12;
+export { a };
